f4dd9d3366127d66b7d154e91c4bb1b3408df0cd
Смирнова Галина <bgalina86@gmail.com>
2024-05-29 12:20:05 +0300

    Lesson28_05_2024

---
+++ src/main/java/com/inopolis/inopolis/Main.java
@@ -0,0 +1,13 @@
+package com.inopolis.inopolis;
+
+public class Main {
+    public static void main(String[] args) {
+
+        var a = 200; //camelCase одно слово и каждое с заглавной
+        var b = 300;
+        System.out.println(a+b);
+        System.out.println( a % b );
+        System.out.println( a * b );
+        System.out.println( a > b );
+    }
+}
