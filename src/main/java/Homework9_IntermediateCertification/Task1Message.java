81f2d05de6a9ef8980cd98679097f220d41cbb6f
Смирнова Галина <bgalina86@gmail.com>
2024-07-04 10:29:46 +0300

    Homework_9 Task1IntermediateCertification

--- src/main/java/Homework9_IntermediateCertification/Task1Message.java
+++ src/main/java/Homework9_IntermediateCertification/Task1Message.java
@@ -1,9 +1,37 @@
 package Homework9_IntermediateCertification;
 
+import java.util.Scanner;
+
 public class Task1Message {
 
-    public static void main(String[] args) {
-        final int lettersAnd3Length=62;
+    static final int lettersAnd3Length = 62;
+    static final int space = 12;
+    static final int lettersTotal = 15;
+    static final int spaceCount = 3;
+
+
+    private static void LengthComparison(int fenceLength, int stringLength) {
+        if (fenceLength > stringLength) {
+            System.out.println(
+                String.format("Надпись мальчика поместится на забор длинной %d см ", fenceLength));
+        } else {
+            System.out.println(
+                String.format("Надпись мальчика не поместится на забор длинной %d см ",
+                    fenceLength));
+        }
     }
 
+    public static void main(String[] args) {
+        int fenceLength;
+        int stringLength = lettersTotal * (lettersAnd3Length / 3) + spaceCount * space;
+
+        //Тест ввод с клавиатуры
+        System.out.print("Введите длинну забора ");
+        fenceLength = Integer.parseInt(new Scanner(System.in).nextLine());
+        LengthComparison(fenceLength, stringLength);
+
+        //Тест проверки условий
+        LengthComparison(50, stringLength);
+        LengthComparison(400, stringLength);
+    }
 }
